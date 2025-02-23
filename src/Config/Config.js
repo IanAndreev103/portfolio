b71fcec959aa125ce322e4b7616d7d8da05b9a56
IanAndreev103 <ianandreev0103@outlook.com>
2025-02-23 15:48:39 -0500

    commit

---
+++ src/Config/Config.js
@@ -0,0 +1,179 @@
+import { BiCodeAlt } from 'react-icons/bi';
+import { IoBrushOutline } from 'react-icons/io5';
+
+export const SkillSet=[
+    {
+        id: "1",
+        name: "C",
+        imgsrc: "images/C.png"
+    },
+    {
+        id: "2",
+        name: "C++",
+        imgsrc: "images/C++.png"
+    },
+    {
+        id: "3",
+        name: "HTML 5",
+        imgsrc: "images/HTML5.png"
+    },
+    {
+        id: "4",
+        name: "CSS 3",
+        imgsrc: "images/CSS3.png"
+    },
+    {
+        id: "5",
+        name: "Javascript",
+        imgsrc: "images/javascript.png"
+    },
+    {
+        id: "6",
+        name: "Bootstrap",
+        imgsrc: "images/Bootstrap.png"
+    },
+    {
+        id: "7",
+        name: "React",
+        imgsrc: "images/React.png"
+    },
+    {
+        id: "8",
+        name: "Material-ui",
+        imgsrc: "images/Material-ui.png"
+    },
+    {
+        id: "9",
+        name: "Sass",
+        imgsrc: "images/SASS.png"
+    },
+    {
+        id: "10",
+        name: "Node.js",
+        imgsrc: "images/nodejs.png"
+    },
+    {
+        id: "11",
+        name: "Express",
+        imgsrc: "images/express.png"
+    },
+    {
+        id: "12",
+        name: "Next.js",
+        imgsrc: "images/Nextjs-logo.svg"
+    },
+    {
+        id: "13",
+        name: "Angular",
+        imgsrc: "images/Angular.png"
+    },
+    {
+        id: "14",
+        name: "Vue.js",
+        imgsrc: "images/Vue.js.png"
+    },
+    {
+        id: "16",
+        name: "MongoDB",
+        imgsrc: "images/mongodb.png"
+    },
+    {
+        id: "17",
+        name: "MySQL",
+        imgsrc: "images/mysql.png"
+    },
+    {
+        id: "18",
+        name: "Github",
+        imgsrc: "images/github.png"
+    },
+
+
+]
+
+export const ProjImg = [
+    {
+        id: "1",
+        name: "ECA Invest(JavaScript)",
+        category: "react",
+        imgsrc: "projectImg/p1.jpg",
+        href: "https://eca-invest.pl/"
+    },
+    {
+        id: "2",
+        name: "Short Way(Laravel, Tailwind, JavaScript, Vue.js)",
+        category: "react",
+        imgsrc: "projectImg/p2.jpg",
+        href: "https://about-vue.yazan-khayal.com"
+    },
+    {
+        id: "3",
+        name: "Magnolia(React, Next.js, Node.js, PostgreSQL)",
+        category: "react",
+        imgsrc: "projectImg/p3.jpg",
+        href: "https://magnoliahq.com/contact.html"
+    },
+    {
+        id: "4",
+        name: "HoWeDid.com - Yii2 project(PHP, HTML5, CSS3, Software Development)",
+        category: "react",
+        imgsrc: "projectImg/p4.png",
+        href: "https://howedid.com"
+    },
+    {
+        id: "5",
+        name: "BoostyLabs(PHP, HTML5, CSS3, Software Development)",
+        category: "react",
+        imgsrc: "projectImg/p5.png",
+        href: "https://boostylabs.com/"
+    },
+    {
+        id: "6",
+        name: "Agro Sciences(Website Design, WordPress, UI/User Experience, Web Development, Website Build)",
+        category: "react",
+        imgsrc: "projectImg/p6.png",
+        href: "https://www.argosciences.com/argoinsights/"
+    },
+    ]
+export const Experience=[
+    
+    {
+        id: "1",
+        date: "Nov 2023 - Sep 2024",
+        name: "Senior Full-Stack Engineer",
+        company: "Dasera",
+        desc: ["Refactored existing code using functional components and React Hooks, reducing bugs in the system by 25%.","Built high performant functional web components with React.js, Redux and GraphQL, decreasing the website loading time by 30%.",
+            "Infused CI/CD into development processes which led to 40% acceleration in release velocity."]
+    },
+    {
+        id: "2",
+        date: "Dec 2021-Oct 2023",
+        name: "Full-Stack web Developer",
+        company: "Hybrid Media",
+        desc: ["Developed and optimized scalable web application using Laravel and Vue.js","Reviewed and refactored codebases for performance,leading to a 20% reduction in load times.",
+            "Managed API integrations with third-party services such as Stripe and AWS.", "Collaborated with design teams to implement Figmamockups into responsive, user friendly interface."]
+    },
+]
+export const Education=[
+
+    {
+        id: 1,
+        date: "Oct 2015-Sep 2018",
+        name: "Donetsk National Technical University",
+        company: "",
+        desc: "Bachelor's Degree of Computer Science"
+    },
+]
+export const Services=[
+    {
+        id: 1,
+         icon: <BiCodeAlt />,
+         name: "Web Development",
+         desc: "",
+    },
+    {   id: 2,
+        icon: <IoBrushOutline/>,
+        name: "Web Design",
+        desc: "",
+    }
+]
